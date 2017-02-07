3acb8b834a165b407a19c0884d7739455c48bafe
a21029delia <a21029delia@gmail.com>
2017-02-07 19:05:21 +0800

    r_prg_midterm_05

---
+++ r_prg_midterm_05.R
@@ -0,0 +1,17 @@
+install.packages("magrittr")
+library(magrittr)
+
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
+# this_year <- as.numeric(format(Sys.Date(), '%Y'))
+# birth_year <- this_year - straw_hat_df$age
+# birth_date_chr <- paste(birth_year, straw_hat_df$birthday, sep = "-")
+# straw_hat_df$birth_date <- as.Date(birth_date_chr)
+# straw_hat_df$birth_date
+
+straw_hat_df$birth_date <- Sys.Date() %>%
+  format(format = "%Y") %>%
+  as.numeric() %>%
+  `-` (straw_hat_df$age) %>%
+  paste(straw_hat_df$birthday, sep = "-") %>%
+  as.Date()
+straw_hat_df$birth_date
